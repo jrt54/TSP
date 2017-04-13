4701e38b9e5c4918b86b7ffbcad4bbc535593fcf
Jeremy Tillay <jtillay@localhost.localdomain>
2017-04-13 15:15:46 -0500

    made continuous, selects useful edges

--- tsp.py
+++ tsp.py
@@ -12,8 +12,6 @@ with open(filename) as input:
 V, E = (int(x) for x in edgeset[0].split())
 print V, E
 
-print edgeset[1].split()
-
 
 dist = {}
 
@@ -27,7 +25,6 @@ dist = {}
 dist = {}
 #read file and create a dictionary of edge, 
 for j in range(1, len(edgeset)): 
-  print j
   begin, end, cost = ([float(x) for x in edgeset[j].split()])
   dist.update({(begin, end): cost})
 
@@ -52,7 +49,7 @@ m = Model()
 #   vars[j, i] = vars[i, j]
 
 
-vars = m.addVars(dist.keys(), obj=dist, vtype=GRB.BINARY, name='e')
+vars = m.addVars(dist.keys(), ub = 1, lb = 0, obj=dist, vtype=GRB.CONTINUOUS, name='e')
 for i,j in vars.keys():
 	 vars[j,i] = vars[i,j] # edge in opposite direction
 
@@ -61,15 +58,13 @@ for i,j in vars.keys():
 m.addConstrs(vars.sum(i,'*') == 2 for i in range(V))
 
 
-m.update()
-
 m._vars = vars
-#m.Params.lazyConstraints = 1
+m.Params.lazyConstraints = 1
 #m.optimize(subtourlim)
 m.optimize()
 
 vals = m.getAttr('x', vars)
-selected = tuplelist((i,j) for i,j in vals.keys() if vals[i,j] > 0.0)
+selected = {(i,j):vals[i, j] for i,j in vals.keys() if vals[i,j] > 0.0}
 print(selected)
 
 
